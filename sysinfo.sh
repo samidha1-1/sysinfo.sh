e9b22fe47bed96da56211f5e87c2dcdcb0689f07
samidha1-1 <samidhawani7@gmail.com>
2025-08-20 15:13:31 +0530

    Initial commit

---
+++ sysinfo.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+
+#"System Info Fetcher Show OS name, kernel version, uptime, CPU, memory, and disk usage. Can be run with ./sysinfo.sh."
+
+echo "================================================================"
+echo                    "SYSTEM INFORMATION"
+echo "================================================================"
+
+#OS NAME 
+echo "OS Name"
+cat /etc/os-release | grep "NAME=|VERSION=" | head -n 2
+
+echo "================================================================"
+
+#KERNEL VERSION
+echo "kernel version"
+uname -r
+
+echo "================================================================"
+
+#SYSTEM UPTIME
+echo "system uptime"
+uptime -p
+
+echo "================================================================"
+
+#CPU INFO 
+echo "cpu info"
+lscpu | grep -e "Model name" 
+
+echo "================================================================="
+
+#MEMORY USAGE
+echo "memory usage"
+free -h
+
+echo "================================================================="
+
+#DISK USAGE
+echo "disk usage"
+df -h 
+
+echo "================================================================="
+
